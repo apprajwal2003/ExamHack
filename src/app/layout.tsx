f5d267273f6ea575376b48c1f3c50c55458a851e
A P Prajwal <apprajwalwork@gmail.com>
2025-05-22 23:51:25 +0530

    font removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "@/components/Footer"; // adjust the path if needed
 
 import "./globals.css";
